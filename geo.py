49bbb34c49fcebad3ce9f4133a93ed435404910d
Paul McLanahan <paul@mclanahan.net>
2012-05-03 10:48:38 -0700

    Add initial foursquare integration.

--- geo.py
+++ geo.py
@@ -1,2 +1,25 @@
+import os
+
 import foursquare
 
+
+FSQ_CLIENT_ID = os.environ.get('FSQ_CLIENT_ID')
+FSQ_CLIENT_SECRET = os.environ.get('FSQ_CLIENT_SECRET')
+FSQ_CATEGORIES = [
+    '4d4b7105d754a06374d81259', # Food
+    '4d4b7105d754a06376d81259', # Nightlife Spot
+    '4d4b7104d754a06370d81259', # Arts & Entertainment
+]
+
+client = foursquare.Foursquare(client_id=FSQ_CLIENT_ID,
+                               client_secret=FSQ_CLIENT_SECRET)
+
+
+def venues_near(ll):
+    """
+    ll = comma separated lat,long. e.g. '36.962452,-122.02446'
+    """
+    return client.venues.search(params={
+        'll': ll,
+        'categoryId': ','.join(FSQ_CATEGORIES),
+    })
